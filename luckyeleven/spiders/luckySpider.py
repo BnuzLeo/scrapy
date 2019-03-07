25f88fd43118bc02b241acb8fa5efce673b089d7
BnuzLeon <769708025@qq.com>
2019-03-08 00:16:41 +0800

    get multiple url response

--- luckyeleven/spiders/luckySpider.py
+++ luckyeleven/spiders/luckySpider.py
@@ -3,13 +3,21 @@ import scrapy
 
 class BlogSpider(scrapy.Spider):
     name = 'lucklySpider'
-    start_urls = ['https://www.jsh365.com/award/gp-gdsyxu/2019-03-06.html']
+    start_urls = ['https://www.jsh365.com/award/gp-gdsyxu/2019-03-06.html',
+                  'https://www.jsh365.com/award/gp-gdsyxu/2019-03-05.html',
+                  'https://www.jsh365.com/award/gp-gdsyxu/2019-03-04.html',
+                  'https://www.jsh365.com/award/gp-gdsyxu/2019-03-03.html',
+                  'https://www.jsh365.com/award/gp-gdsyxu/2019-03-02.html',
+                  'https://www.jsh365.com/award/gp-gdsyxu/2019-03-01.html'
+                  ]
 
     def parse(self, response):
+        print(response.url)
         for tab in response.css('._tab'):
-            i = 0
+            i = 1
             for item in tab.css('tr>td>div'):
-                if i == 0:
+                if i % 6 == 0:
                     print(item.css('::text').get())
                 else:
                     print(item.css('::text').get()),
+                i = i + 1
